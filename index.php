3fbb16b22250ab9bfea748ed83a282f7b13be14c
latifi777 <abdelalilatifi3@gmail.com>
2024-12-17 15:54:34 +0100

    en commencant par l-interface

--- index.php
+++ index.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Improved Form</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
-    
-</body>
-</html>
+<body class="bg-[url('pexels-pixabay-315938.jpg')] bg-cover bg-no-repeat bg-center text-zinc-50 bg-fixed">
+    <!-- Header -->
+    <header class="flex justify-between items-center px-6 py-4">
+        <h1 class="text-2xl font-extrabold">LOGO</h1>
+        <nav>
+            <ul class="flex gap-x-6">
+                <li class="text-xl font-bold">HELLO</li>
+                <li class="text-xl font-bold">THE</li>
+                <li class="text-xl font-bold">FUCKING</li>
+                <li class="text-xl font-bold">WORLD</li>
+            </ul>
+        </nav>
+        <div>
+            <button id="sign_in" class="p-2 text-xl font-bold rounded-lg hover:bg-violet-600">Sign In</button>
+            <button id="sign_up" class="p-2 text-xl font-bold rounded-lg hover:bg-violet-600">Sign Up</button>
+        </div>
+    </header>
+
+   
