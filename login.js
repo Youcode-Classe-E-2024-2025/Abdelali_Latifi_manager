c9fd1b6016191bc59c10c859e1ecc8f203629185
latifi777 <abdelalilatifi3@gmail.com>
2024-12-18 11:43:04 +0100

    la validation des inputs

---
+++ login.js
@@ -0,0 +1,28 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("first-name").value;
+    const password = document.getElementById("password").value;
+
+    const nameRegex = /^[A-Za-z]{2,}$/;
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+
+    let isValid = true;
+
+    if (!nameRegex.test(name)) {
+        isValid = false;
+        alert('Name must only contain letters and be at least 2 characters long')
+    } else {
+        document.getElementById("nameError").classList.add("hidden");
+    }
+
+    if (!passwordRegex.test(password)) {
+        isValid = false;
+        alert('Password must be at least 8 characters long and contain at least one letter and one number.')
+    } 
+
+    if (isValid) {
+        alert("Form submitted successfully!");
+       
+    }
+});
---
+++ registre.js
@@ -0,0 +1,39 @@
+document.getElementById("signUpForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    const nameRegex = /^[A-Za-z]{2,}$/; 
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+    const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+    let isValid = true;
+
+    // Validate First Name
+    if (!nameRegex.test(firstName)) {
+        isValid = false;
+        alert('First name must contain only letters and be at least 2 characters long.');
+    } 
+    // Validate Last Name
+    if (!nameRegex.test(lastName)) {
+        isValid = false;
+        alert('Last name must contain only letters and be at least 2 characters long.');
+
+    } 
+    // Validate Email
+    if (!emailRegex.test(email)) {
+        isValid = false;
+        alert('Invalid email format.');
+    } 
+    // Validate Password
+    if (!passwordRegex.test(password)) {
+        isValid = false;
+        alert('Password must be at least 8 characters long and contain at least one letter and one number.');
+    } 
+    // If valid, submit the form
+    if (isValid) {
+        alert("Form submitted successfully!");
+    }
+});
